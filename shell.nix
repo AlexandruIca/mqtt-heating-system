90423a3f6ade6c46566f805f05debf7cbb6e8366
Ică Alexandru-Gabriel <alexandruica703@gmail.com>
2021-10-16 21:05:56 +0300

    Add a basic nix development environment (#1)

---
+++ shell.nix
@@ -0,0 +1,17 @@
+let
+    pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/ee90403e147b181300dffca5b0afa405e14f1945.tar.gz")) {};
+in pkgs.mkShell {
+    name = "cpp-environment";
+    buildInputs = [
+        pkgs.gcc11
+	pkgs.ninja
+	pkgs.gnumake
+	pkgs.cmake
+	pkgs.cmake-format
+	pkgs.clang-tools
+	pkgs.ccache
+	pkgs.doxygen
+	pkgs.python39
+    ];
+    shellHook = ''source ~/.bashrc'';
+}
