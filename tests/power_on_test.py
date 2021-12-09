754a93c0a882a1bc51760b41868f43b5afcad9a9
Ică Alexandru-Gabriel <alexandruica703@gmail.com>
2021-12-09 17:24:44 +0200

    Check if payload is being sent to the server on `power/` topic (#4)

---
+++ tests/power_on_test.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+
+
+def on_connect(client, userdata, flags, rc):
+    print(f'Connected with result: {rc}')
+    client.publish("power", payload="on")
+
+
+def on_message(client, userdata, msg):
+    print(f'Received PUBLISH: topic={msg.topic}, payload: {msg.payload}')
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883, 60)
+
+client.loop_forever()
