c49351c31dfe5b24d63624423283e7292f8329e9
Jungle335 <mfotbaliatorul@gmail.com>
2021-12-13 11:43:26 +0200

    Add a simple test for changing temperature functionality (#5)

---
+++ tests/temperature_test.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+from common import *
+
+
+def on_connect(client, userdata, flags, rc):
+    print(f'Connected with result: {rc}')
+    (topic, payload) = request_to_payload(Request.TEMPERATURE_UP)
+    client.publish(topic, payload)
+    (topic, payload) = request_to_payload(Request.TEMPERATURE_DOWN)
+    client.publish(topic, payload)
+
+
+def on_message(client, userdata, msg):
+    print(f'Received PUBLISH: topic={msg.topic}, payload: {msg.payload}')
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(HOST, PORT, KEEPALIVE)
+
+client.loop_forever()
