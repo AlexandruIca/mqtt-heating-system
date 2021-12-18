7f43952a236a42d58a090516605f77d3eb0a339f
Jungle335 <mfotbaliatorul@gmail.com>
2021-12-18 20:30:12 +0200

    Added new statistics feature (#9)

--- server/common.py
+++ server/common.py
@@ -8,7 +8,10 @@ POWER_TOPIC: str = 'power'
 TEMP_TOPIC: str = 'temperature'
 WARNINGS_TOPIC: str = 'warning'
 TEMP_WARNINGS_SUBTOPIC: str = 'temperature'
-
+STATISTICS_TOPIC: str = 'statics'
+STATISTICS_WATER_USAGE_GETTER_SUBTOPIC: str = 'water_getter'
+STATISTICS_WATER_USAGE_SETTER_SUBTOPIC: str = 'water_setter'
+STATISTICS_GAS_USAGE_SUBTOPIC: str = 'gas'
 
 class Request(Enum):
     INVALID = 0
@@ -16,7 +19,8 @@ class Request(Enum):
     POWER_OFF = 2
     TEMPERATURE_UP = 3
     TEMPERATURE_DOWN = 4
-    WARNING = 5
+    WARNING = 5    
+    WATER_STATISTICS = 6
 
 
 def on_power_request(state, status, f):
@@ -36,6 +40,10 @@ def on_change_temperature_request(state, f, sign, how_much):
             state.temperature -= how_much
     f()
 
+def on_statistics(state, f):
+    state.client.publish(f'{STATISTICS_TOPIC}/{STATISTICS_WATER_USAGE_SETTER_SUBTOPIC}', str(state.water_usage))
+    f()
+
 request_map = {
     Request.INVALID: lambda _1, _2, f: f(),
     Request.POWER_ON: lambda state, _, f: on_power_request(state, True, f),
@@ -43,6 +51,7 @@ request_map = {
     Request.TEMPERATURE_UP: lambda state, _, f: on_change_temperature_request(state, f, '+', 0.5),
     Request.TEMPERATURE_DOWN: lambda state, _, f: on_change_temperature_request(state, f, '-', 0.5),
     Request.WARNING: lambda state, payload, f: state.on_error(topic, payload),
+    Request.WATER_STATISTICS: lambda state, _, f: on_statistics(state, f),
 }
 
 
@@ -61,6 +70,8 @@ def payload_to_request(topic: str, payload: str):
             return Request.TEMPERATURE_DOWN
     elif topic.startswith(WARNINGS_TOPIC):
         return Request.WARNING
+    elif topic.startswith(STATISTICS_TOPIC):
+        return Request.WATER_STATISTICS
     else:
         return Request.INVALID
 
@@ -76,7 +87,8 @@ def request_to_payload(req: Request, payload = None):
         return (TEMP_TOPIC, 'down')
     elif req == Request.WARNING:
         return (WARNINGS_TOPIC, payload)
-
+    elif req == Request.WATER_STATISTICS:
+        return (f'{STATISTICS_TOPIC}/{STATISTICS_WATER_USAGE_GETTER_SUBTOPIC}', payload)
 
 def default_callback():
     pass
@@ -88,6 +100,8 @@ class State:
         self.temperature = 20 
         self.client = client
         self.on_error = on_error
+        # cata apa a consumat in ultimele x luni
+        self.water_usage = [10, 50, 32, 24]
 
     def process_request(self, req: Request, callback=default_callback, payload=None):
         request_map[req](self, payload, callback)
---
+++ tests/statistics_feature_test.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mqtt
+from common import *
+
+
+def on_connect(client, userdata, flags, rc):
+    print(f'Connected with result: {rc}')    
+    client.subscribe(f'{WARNINGS_TOPIC}/#')
+    client.subscribe(f'{STATISTICS_TOPIC}/#')
+    (topic, payload) = request_to_payload(Request.WATER_STATISTICS)
+    client.publish(topic, payload)
+
+
+def on_message(client, userdata, msg):
+    print(f'Received PUBLISH: topic={msg.topic}, payload: {msg.payload}')
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(HOST, PORT, KEEPALIVE)
+
+client.loop_forever()
