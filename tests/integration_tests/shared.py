d6e73549e3c694214b77d09193dbc5b96aa513f3
Ică Alexandru-Gabriel <alexandruica703@gmail.com>
2022-02-02 17:07:54 +0200

    Call the `requests.post` method correctly with json body (#3)

--- tests/integration_tests/shared.py
+++ tests/integration_tests/shared.py
@@ -1,5 +1,6 @@
 import requests
+import json
 
 
-def post_to(route):
-    return requests.post(f'http://localhost:5000/{route}', {}).text
+def post_to(route, body={}):
+    return json.loads(requests.post(f'http://localhost:5000/{route}', json=body).text)
