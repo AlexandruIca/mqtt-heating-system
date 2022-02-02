f9452c9071f4c4a7eb0555e4d67a8c29f137982c
Ică Alexandru-Gabriel <alexandruica703@gmail.com>
2022-02-02 17:09:12 +0200

    Update integration tests with new JSON result from POSTs (#3)

--- tests/integration_tests/temperature_tests.py
+++ tests/integration_tests/temperature_tests.py
@@ -1,13 +1,31 @@
 from shared import post_to
 
+
+result = None
 output = 0
+
+
 for i in range(100):
-    output = float(post_to('temperature_up'))
+    result = post_to('temperature_up')
+    assert(result['type'] == 'number')
+
+    if 'error' in result and len(result['error']) > 0:
+        break
+
+    output = float(result['value'])
+
 
 assert(output == 30)
 
+
 for i in range(100):
-    output = float(post_to('temperature_down'))
+    result = post_to('temperature_down')
+    assert(result['type'] == 'number')
+
+    if 'error' in result and len(result['error']) > 0:
+        break
+
+    output = float(result['value'])
 
 
 assert(output == 18)
--- tests/integration_tests/water_temperature_tests.py
+++ tests/integration_tests/water_temperature_tests.py
@@ -1,13 +1,31 @@
 from shared import post_to
 
+
+result = None
 output = 0
+
+
 for i in range(100):
-    output = float(post_to('water_temperature_up'))
+    result = post_to('water_temperature_up')
+    assert(result['type'] == 'number')
+
+    if 'error' in result and len(result['error']) > 0:
+        break
+
+    output = float(result['value'])
+
 
 assert(output == 90)
 
+
 for i in range(200):
-    output = float(post_to('water_temperature_down'))
+    result = post_to('water_temperature_down')
+    assert(result['type'] == 'number')
+
+    if 'error' in result and len(result['error']) > 0:
+        break
+
+    output = float(result['value'])
 
 
 assert(output == 20)
